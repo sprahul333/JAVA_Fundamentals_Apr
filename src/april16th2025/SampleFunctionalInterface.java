a20f7d1bf3a60d4de2da636c698ab211b52d46d7
sprahul333 <sprahul333@gmail.com>
2025-04-19 19:29:48 +0530

    Deleting existing files

--- src/april16th2025/SampleFunctionalInterface.java
+++
@@ -1,7 +0,0 @@
-package april16th2025;
-
-//80 percent of JAVA works on functional interfaces
-@FunctionalInterface
-public interface SampleFunctionalInterface {
-    void display();
-}
--- src/march31st2025/SampleProgram_JDK24Version.java
+++
@@ -1,9 +0,0 @@
-//package march31st2025;
-
-import static java.io.IO.println;
-
-void main()
-{
-//    System.out.println("Hello World"); JDK 21 Style
-    println("Hello World"); //JDK 24
-}
